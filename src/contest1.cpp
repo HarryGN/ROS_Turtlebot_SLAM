4449dd308c141f5d41a1d996f517ef3e7fa73d71
pxrcy301 <xpercy.wu@gmail.com>
2025-02-05 22:14:06 -0800

    Changes to be committed: 	modified:   src/contest1.cpp

--- src/contest1.cpp
+++ src/contest1.cpp
@@ -31,7 +31,7 @@ float kn_r = 0.7;
 float minAngular = 15; // Degrees per second
 float maxAngular = 90; // Degrees per second
 
-float navigationTolerance = 0.2;
+float navigationTolerance = 0.05;
 float kp_n = 0.02;
 float kn_n = 0.5;
 float minLinear = 0.1;
@@ -46,10 +46,46 @@ bool rightBumperPressed;
 #pragma endregion
 
 #pragma region Laser
-float leftDistance = 0.0, rightDistance = 0.0, centerDistance = 0.0, minDistance = 0.0;
-uint16_t nLasers=0, desiredNLasers=0, desiredAngle=5;
+struct DistancesStruct{
+    float leftRay;
+    float leftRayPrev;
+    float leftHorz;
+    float leftHorzPrev;
+    float leftVert;
+    float leftVertPrev;
+
+    float frontRay;
+    float frontRayPrev;
+
+    float rightRay;
+    float rightRayPrev;
+    float rightHorz;
+    float rightHorzPrev;
+    float rightVert;
+    float rightVertPrev;
+
+    float min;
+    float minPrev;
+};
+
+DistancesStruct distances;
+
+uint16_t nLasers=0;
+float fullAngle = 57.0;
 
 #pragma endregion
+
+#pragma region Laser Obstacle Avoidance
+
+
+#pragma endregion
+
+
+void orthogonalizeRay(int ind, int nLasers, float distance, float &horz_dist, float &front_dist){
+    float angle = (float) ind / (float) nLasers * fullAngle + 90 - fullAngle/2;
+    horz_dist = std::abs(distance * std::cos(Deg2Rad(angle)));
+    front_dist = std::abs(distance * std::sin(Deg2Rad(angle)));
+}
 
 void bumperCallback(const kobuki_msgs::BumperEvent::ConstPtr& msg){
     bumper[msg->bumper] = msg->state;
@@ -61,39 +97,86 @@ void bumperCallback(const kobuki_msgs::BumperEvent::ConstPtr& msg){
 
 void laserCallback(const sensor_msgs::LaserScan::ConstPtr& msg){
     nLasers = (msg->angle_max - msg->angle_min) / msg->angle_increment;
+    // 1. Update previous values
+    distances.leftRayPrev = distances.leftRay;
+    distances.leftHorzPrev = distances.leftHorz;
+    distances.leftVertPrev = distances.leftVert;
 
-    // Get the indices for first, middle, and last readings
-    uint16_t  right_idx = 0;                 // First reading (left)
-    uint16_t  front_idx = nLasers / 2;      // Middle reading (front)
-    uint16_t  left_idx = nLasers - 1;      // Last reading (right)
+    distances.frontRayPrev = distances.frontRay;
 
-    leftDistance = msg->ranges[left_idx];
-    centerDistance = msg->ranges[front_idx];
-    rightDistance = msg->ranges[right_idx];
+    distances.rightRayPrev = distances.rightRay;
+    distances.rightHorzPrev = distances.rightHorz;
+    distances.rightVertPrev = distances.rightVert;
 
-    minDistance = msg->ranges[0];
-    for (int i=0; i< nLasers; i++){
-        // msg->ranges[i] is non-NaN, update minDistance
-        if(msg->ranges[i] == msg->ranges[i]){
-            minDistance = std::min(minDistance, msg->ranges[i]);
+    distances.minPrev = distances.min;
+
+    // ROS_INFO("CURR %.2f %.2f %.2f %.2f %.2f %.2f %.2f %.2f", distances.leftRay, distances.leftHorz, distances.leftVert, distances.frontRay, distances.rightRay, distances.rightHorz, distances.rightVert, distances.min);
+    // ROS_INFO("PREV %.2f %.2f %.2f %.2f %.2f %.2f %.2f %.2f", distances.leftRayPrev, distances.leftHorzPrev, distances.leftVertPrev, distances.frontRayPrev, distances.rightRayPrev, distances.rightHorzPrev, distances.rightVertPrev, distances.minPrev);
+
+
+    // 2. Get the indices for first, middle, and last readings
+    uint16_t  rightInd = 0;               // First reading (right)
+    uint16_t  frontInd = nLasers / 2;     // Middle reading (front)
+    uint16_t  leftInd = nLasers - 1;      // Last reading (left)
+
+    // 3. Find the closes non-nan value for right, front, and left
+    distances.leftRay= msg->ranges[leftInd];
+    distances.frontRay= msg->ranges[frontInd];
+    distances.rightRay = msg->ranges[rightInd];
+
+    // 3a Left
+    while(std::isnan(distances.leftRay)){
+        distances.leftRay = msg->ranges[leftInd];
+        leftInd--;
+
+        if(leftInd < frontInd){
+            distances.leftRay = 0.25;
+            break;
         }
-
-        // msg->ranges[i] is NaN, set minDistance to 0
-        // else{
-        //     minDistance = 0;
-        //     break;
-        // }
     }
 
+    // 3b Right
+    while(std::isnan(distances.rightRay)){
+        distances.rightRay = msg->ranges[rightInd];
+        rightInd++;
 
-    // ROS_INFO(msg->ranges);
+        if(rightInd > frontInd){
+            distances.rightRay=0.25;
+            break;
+        }
+    }
 
-    // for(int i = 0; i < nLasers; i++){
-    //     ROS_INFO("dist%u : %.2f", i, msg->ranges[i]);
-    // }
+    // 3c Front
+    int i = 1;
+    bool odd = true;
 
-    // ROS_INFO("L/C/R/M Distances %.2f/%.2f/%.2f/%.2f", leftDistance, centerDistance, rightDistance, minDistance);
+    while(std::isnan(distances.frontRay)){
+        distances.frontRay = msg->ranges[frontInd+i];
+        if(odd){
+            odd = false;
+            i = -(i);
+        }
+        else{
+            odd = true;
+            i = -(i+1);
+        }
+    }
+
+    frontInd += i;
+
+    // 4. Calculate Orthogonal (Horz/Vert) for Left and Right
+    // 4a Left
+    orthogonalizeRay(leftInd, nLasers, distances.leftRay, distances.leftHorz, distances.leftVert);
+    
+    // 4b Right
+    orthogonalizeRay(rightInd, nLasers, distances.rightRay, distances.rightHorz, distances.rightVert);
+
+    // 5. Calculate min Distance
+    distances.min = std::min(std::min(distances.rightRay, distances.frontRay), distances.leftRay);
+
+
 }
+
 
 void callbackOdom(const nav_msgs::Odometry::ConstPtr& msg){
     posX = msg->pose.pose.position.x;
@@ -136,6 +219,16 @@ float computeAngular(float targetHeading, float currentYaw){
 
     // Calculate proportional component and then calculate angularDeg based on if it is negative or positive
     float proportional = kp_r*(targetHeading-currentYaw);
+
+    if(proportional > 180){
+        proportional -= 360;
+    }
+
+    else if(proportional < -180){
+        proportional += 360;
+    }
+
+
     if(proportional < 0){
         angularDeg = (float) -1*pow(-1*proportional, kn_r);
     }
@@ -158,6 +251,14 @@ float computeLinear(float tgtX, float tgtY, float posX, float posY){
     return localLinear;
 }
 
+void computeAdvanceCoordinate(float distance, float yaw, float posX, float posY, float &targetX, float &targetY){
+    float dx = distance * std::cos(Deg2Rad(yaw));
+    float dy = distance * std::sin(Deg2Rad(yaw));
+
+    targetX = posX + dx;
+    targetY = posY + dy;
+}
+
 void rotateToHeading(float targetHeading, geometry_msgs::Twist &vel, ros::Publisher &vel_pub){
     ROS_INFO("ROTATE TO HEADING CALLED");
     ros::spinOnce();
@@ -166,10 +267,11 @@ void rotateToHeading(float targetHeading, geometry_msgs::Twist &vel, ros::Publis
     while(abs(targetHeading - yaw) > rotationTolerance){
 
         angular = computeAngular(targetHeading, yaw);
+        linear = 0;
 
         ros::spinOnce();
         vel.angular.z = angular;
-        vel.linear.x = 0;
+        vel.linear.x = linear;
         vel_pub.publish(vel);
 
         ROS_INFO("Target/Current Yaw: %f/%f degs | Setpoint: %f degs/s", targetHeading, yaw, Rad2Deg(angular));
@@ -182,12 +284,14 @@ void rotateToHeading(float targetHeading, geometry_msgs::Twist &vel, ros::Publis
 }
 
 
-void navigateToPosition(float x, float y, geometry_msgs::Twist &vel, ros::Publisher &vel_pub){
+void navigateToPosition(float tgtX, float tgtY, geometry_msgs::Twist &vel, ros::Publisher &vel_pub){
     ROS_INFO("navigateToPosition() CALLED");
     ros::spinOnce();
+
+    int counter = 0;
     
-    float dx = x-posX;
-    float dy = y-posY;
+    float dx = tgtX-posX;
+    float dy = tgtY-posY;
     float d = (float) sqrt(pow(dx, 2) + pow(dy, 2));
 
     // Set and rotate to initial heading
@@ -197,19 +301,27 @@ void navigateToPosition(float x, float y, geometry_msgs::Twist &vel, ros::Publis
     // While loop until robot gets there
     while(d > navigationTolerance){
         ros::spinOnce();
-        dx = x-posX;
-        dy = y-posY;
+        dx = tgtX-posX;
+        dy = tgtY-posY;
+        d = (float) sqrt(pow(dx, 2) + pow(dy, 2));
 
-        linear = computeLinear(x, y, posX, posY);
+        linear = computeLinear(tgtX, tgtY, posX, posY);
 
         targetHeading = Rad2Deg(atan2(dy, dx));
+        minAngular = 0;
         angular = computeAngular(targetHeading, yaw);
+        minAngular = 15;
 
         vel.angular.z = angular;
         vel.linear.x = linear;
         vel_pub.publish(vel);
 
-        ROS_INFO("Tgt X/Y: %f/%f | Pos X/Y: %f/%f | Lin/Ang: %f/%f", x, y, posX, posY, linear, Rad2Deg(angular));
+        counter ++;
+        
+        if(counter%500000 == 0){
+            ROS_INFO("Tgt X/Y: %f/%f | Pos X/Y: %f/%f | Lin/Ang: %f/%f", tgtX, tgtY, posX, posY, linear, Rad2Deg(angular));
+        }
+        
     }
 
     linear = 0;
@@ -223,23 +335,34 @@ void navigateToPosition(float x, float y, geometry_msgs::Twist &vel, ros::Publis
 
 void navigateToPositionSmart(float tgtX, float tgtY, geometry_msgs::Twist &vel, ros::Publisher &vel_pub){
     ROS_INFO("navigateToPositionSmart(...) called");
+    
+    // Setup
     ros::spinOnce();
     bool followingWall = false;
     bool obstacleAhead = false;
     float dx, dy, d;
     float exitThreshold = 0.3;
 
+    float sideOpenDistThreshold = 0.7;
+
+    float advanceDistance;
+    float headingThreshold = 2; // Degrees
+
     std::string followingMode = "none";
 
-    //One-time major heading adjustment
+    // One-time major heading adjustment to face target
     dx = tgtX-posX;
     dy = tgtY-posY;
 
     float targetHeading = Rad2Deg(atan2(dy, dx));
     rotateToHeading(targetHeading, vel, vel_pub);
+    uint16_t counter = 0;
 
+    // Loop
     while(true){
         ros::spinOnce();
+        counter ++;
+        // ROS_INFO("LEFT HORZ/RAY: %.2f/%.2f", distances.leftHorz, distances.leftRay);
 
         // Recalculate dx, dy, d, targetHeading
         dx = tgtX-posX;
@@ -249,20 +372,20 @@ void navigateToPositionSmart(float tgtX, float tgtY, geometry_msgs::Twist &vel, 
 
 
         // Exit Condition
-        if(d<exitThreshold){
+        if(d < exitThreshold){
             return;
         }
 
 
         // 1. Check if there are obstacles ahead
-        if(minDistance < 0.51){
+        if(distances.min < 0.51){
             obstacleAhead = true;
         }
         else {
             obstacleAhead = false;
         }
         
-        // 2a. If no obstacles ahead, calculate linear/angular and continue moving to target
+        // 2a. NO OBSTACLE MOVEMENT | If no obstacles ahead, calculate linear/angular and continue moving to target
         if(!obstacleAhead && !followingWall){
 
 
@@ -271,17 +394,21 @@ void navigateToPositionSmart(float tgtX, float tgtY, geometry_msgs::Twist &vel, 
             minAngular = 0;
             angular = computeAngular(targetHeading, yaw);
             minAngular = 15;
-            // angular = 0;
-            ROS_INFO("Target/Current Yaw: %f/%f degs | Setpoint: %f degs/s", targetHeading, yaw, Rad2Deg(angular));
-        }
 
-        // 2b. If obstacles ahead, set followingWall to true and followingMode to left or right.
+            if(counter %50000 == 0){
+                ROS_INFO("nTPS().2a | Tgt X/Y: %f/%f | Pos X/Y: %f/%f | Lin/Ang: %f/%f", tgtX, tgtY, posX, posY, linear, Rad2Deg(angular));
+            }
+            
+        }
+        
+
+        // 2b. WALL FOLLOWING SETUP | If obstacles ahead, set followingWall to true and followingMode to left or right.
         else if(obstacleAhead && !followingWall){
             followingWall = true;
 
-            ROS_INFO("R/L Distance %.2f/%.2f", rightDistance, leftDistance);
+            ROS_INFO("R/L Horz Distance %.2f/%.2f", distances.rightHorz, distances.leftHorz);
 
-            if(rightDistance > leftDistance){
+            if(distances.rightRay > distances.leftRay){
                 followingMode = "left";
                 ROS_INFO("FOLLOWING WALL ON LEFT");
             }
@@ -294,26 +421,25 @@ void navigateToPositionSmart(float tgtX, float tgtY, geometry_msgs::Twist &vel, 
 
 
 
-        // 3. If supposed to be following wall, continue following wall until exit condition triggers
+        // 3. WALL FOLLOWING | If supposed to be following wall, continue following wall until exit condition triggers
         if(followingWall){
-            angular = 0;
-            linear = 0;
 
-            // Right distance is greater than left -> follow wall on left
+
+            // 3A FOLLOW WALL ON LEFT | Right distance is greater than left -> follow wall on left
             if(followingMode == "left"){
-                // ROS_INFO("FOLLOWING WALL ON LEFT");
-                if (centerDistance > 1.0 && !std::isnan(centerDistance) && !std::isnan(leftDistance) && !std::isnan(rightDistance)) {
+                // Wall following kinematics computation
+                if (distances.frontRay > 1.0) {
     
                     const double k = 0.15;   // Scaling factor for angular velocity
-                    const double alpha = 1.5; // Exponential growth/decay rate
+                    const double alpha = 2.3; // Exponential growth/decay rate
                     float targetDistance = 0.9;
 
-                    if (leftDistance < targetDistance) {
-                        angular = -k * (1-exp(-alpha * leftDistance)); // Exponential decay for left turns
+                    if (distances.leftRay < targetDistance) {
+                        angular = -k * (1-exp(-alpha * distances.leftRay)); // Exponential decay for left turns
                         linear = 0.1;                                   // Set a constant forward speed
                     } 
-                    else if (leftDistance > targetDistance) {
-                        angular = k * (1-exp(-alpha * leftDistance));  // Exponential decay for right turns
+                    else if (distances.leftRay > targetDistance) {
+                        angular = k * (1-exp(-alpha * distances.leftRay));  // Exponential decay for right turns
                         linear = 0.1;                                   // Set a constant forward speed
                     } 
                     else {
@@ -326,10 +452,37 @@ void navigateToPositionSmart(float tgtX, float tgtY, geometry_msgs::Twist &vel, 
                     linear = 0.04;
                     angular = -0.26;                     // Rotate in place to adjust to right
                 }
+
+                // Check if sudden opening
+                if(distances.leftHorzPrev < sideOpenDistThreshold && distances.leftHorz > sideOpenDistThreshold){
+                    ROS_INFO("OPENING DETECTED WHILE FOLLOWING WALL ON LEFT");
+                    followingMode = "leftAdvanced";
+                    advanceDistance = distances.leftVertPrev + 0.1;
+                    float tgtX;
+                    float tgtY;
+
+                    computeAdvanceCoordinate(advanceDistance, yaw, posX, posY, tgtX, tgtY);
+                    navigateToPosition(tgtX, tgtY, vel, vel_pub);
+                }
+
+
             }
 
-            // Left distance is greater than right -> follow wall on right
-            else{
+            else if (followingMode == "leftAdvanced"){
+                linear = 0.1;
+                angular = Deg2Rad((float) 11.4);
+                
+                // Return to normal navigation if turned to the correct heading
+                if(std::abs(targetHeading - yaw) < headingThreshold && distances.frontRay > 0.51){
+                    followingMode = "none";
+                    followingWall = false;
+
+                }
+                
+            }
+
+            // 3B FOLLOW WALL ON RIGHT | Left distance is greater than right -> follow wall on right
+            else if (followingMode == "right"){
                 //ROS_INFO("FOLLOWING WALL ON RIGHT");
             }
         }
@@ -373,23 +526,24 @@ int main(int argc, char **argv)
         dBumperEventTotal += dBumperEvent[i];
     }
 
-
     angular = 0.0;
     linear = 0.0;
 
     // rotateToHeading(90, vel, vel_pub);
     // rotateEndlessly(vel, vel_pub);
-    navigateToPositionSmart(-1.929,1.346, vel, vel_pub);
-    navigateToPositionSmart(-1.7069999999999999,-1.0, vel, vel_pub);
-    navigateToPositionSmart(-0.8680000000000001,1.4365, vel, vel_pub);
-    navigateToPositionSmart(-0.3763333333333332,-1.1406666666666667, vel, vel_pub);
-    navigateToPositionSmart(0.19299999999999984,1.5270000000000001, vel, vel_pub);
-    navigateToPositionSmart(0.9543333333333335,-1.2813333333333332, vel, vel_pub);
-    navigateToPositionSmart(1.2539999999999998,1.6175, vel, vel_pub);
-    navigateToPositionSmart(2.285,-1.422, vel, vel_pub);
-    navigateToPositionSmart(2.3149999999999995,1.708, vel, vel_pub);
+    // navigateToPositionSmart(-1.929,1.346, vel, vel_pub);
+    // navigateToPositionSmart(-1.7069999999999999,-1.0, vel, vel_pub);
+    // navigateToPositionSmart(-0.8680000000000001,1.4365, vel, vel_pub);
+    // navigateToPositionSmart(-0.3763333333333332,-1.1406666666666667, vel, vel_pub);
+    // navigateToPositionSmart(0.19299999999999984,1.5270000000000001, vel, vel_pub);
+    // navigateToPositionSmart(0.9543333333333335,-1.2813333333333332, vel, vel_pub);
+    // navigateToPositionSmart(1.2539999999999998,1.6175, vel, vel_pub);
+    // navigateToPositionSmart(2.285,-1.422, vel, vel_pub);
+    // navigateToPositionSmart(2.3149999999999995,1.708, vel, vel_pub);
 
-    // navigateToPositionSmart(0, 1.5, vel, vel_pub);
+    navigateToPositionSmart(-2, 0, vel, vel_pub);
+
+    return 0;
 
     while(ros::ok() && secondsElapsed <= 480) {
         ros::spinOnce();
