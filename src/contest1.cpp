87b539b8cbb1b09114bf19fd9053c7e23ae539b9
pxrcy301 <xpercy.wu@gmail.com>
2025-02-07 15:30:06 -0800

    Changes to be committed: 	modified:   src/contest1.cpp

--- src/contest1.cpp
+++ src/contest1.cpp
@@ -89,6 +89,7 @@ float fullAngle = 57.0;
 std::vector<std::array<float,2>> zigZagPoints;
 #pragma endregion
 
+void checkBumper(geometry_msgs::Twist &vel, ros::Publisher &vel_pub);
 
 void orthogonalizeRay(int ind, int nLasers, float distance, float &horz_dist, float &front_dist){
     float angle = (float) ind / (float) nLasers * fullAngle + 90 - fullAngle/2;
@@ -329,6 +330,7 @@ void navigateToPosition(float tgtX, float tgtY, geometry_msgs::Twist &vel, ros::
         dx = tgtX-posX;
         dy = tgtY-posY;
         d = (float) sqrt(pow(dx, 2) + pow(dy, 2));
+        checkBumper(vel, vel_pub);
 
         linear = computeLinear(tgtX, tgtY, posX, posY);
 
@@ -356,6 +358,94 @@ void navigateToPosition(float tgtX, float tgtY, geometry_msgs::Twist &vel, ros::
     vel_pub.publish(vel);
 
     ROS_INFO("Successful exit from navigateToPosition.");
+}
+
+void handleBumperPressed(float turnAngle, geometry_msgs::Twist &vel, ros::Publisher &vel_pub){
+    ROS_INFO("handleBumperPressed() called...");
+    float reverseDistance = 0.2;
+    float forwardDistance = reverseDistance / std::cos(Deg2Rad(turnAngle)) * 0.9;
+
+    float exitDistanceThreshold = 0.02;
+
+    // 1. Reverse
+    ROS_INFO("handleBumperPressed() | Reversing...");
+    float x0 = posX;
+    float y0 = posY;
+    
+    float dx;
+    float dy;
+    float d = 0;
+
+    while((d-reverseDistance) < exitDistanceThreshold){
+        ros::spinOnce();
+
+        linear = -0.1;
+        angular = 0;
+
+        dx = posX-x0;
+        dy = posY-y0;
+        d = (float) sqrt(pow(dx, 2) + pow(dy, 2));
+
+        vel.angular.z = angular;
+        vel.linear.x = linear;
+        vel_pub.publish(vel);
+    }
+
+
+    // 2. Turn
+    if(turnAngle == 0){ // If center bumper was pressed this is called
+        if(distances.leftRay > distances.rightRay){
+            turnAngle = 45;
+        }
+
+        else{
+            turnAngle = -45;
+        }
+
+    }
+    ROS_INFO("handleBumperPressed() | Turning...");
+    rotateToHeading(yaw + turnAngle, vel, vel_pub);
+
+    // 3. Drive Forward
+    ROS_INFO("handleBumperPressed() | Advancing...");
+    x0 = posX;
+    y0 = posY;
+    dx = 0;
+    dy = 0;
+    d = 0;
+    while((d-forwardDistance) < exitDistanceThreshold){
+        ros::spinOnce();
+
+        linear = 0.1;
+        angular = 0;
+
+        dx = posX-x0;
+        dy = posY-y0;
+        d = (float) sqrt(pow(dx, 2) + pow(dy, 2));
+
+
+
+        vel.angular.z = angular;
+        vel.linear.x = linear;
+        vel_pub.publish(vel);
+    }
+
+    // 4. Turn Back
+    ROS_INFO("handleBumperPressed() | Correcting yaw...");
+    rotateToHeading(yaw - turnAngle, vel, vel_pub);
+
+
+
+    ROS_INFO("handleBumperPressed() | END");
+    linear = 0;
+    angular = 0;
+
+    vel.angular.z = angular;
+    vel.linear.x = linear;
+    vel_pub.publish(vel);
+
+    return;
+
 }
 
 void navigateToPositionSmart(float tgtX, float tgtY, geometry_msgs::Twist &vel, ros::Publisher &vel_pub){
@@ -395,6 +485,7 @@ void navigateToPositionSmart(float tgtX, float tgtY, geometry_msgs::Twist &vel, 
         d = (float) sqrt(pow(dx, 2) + pow(dy, 2));
         targetHeading = Rad2Deg(atan2(dy, dx));
 
+        checkBumper(vel, vel_pub);
 
         // Exit Condition
         if(d < exitThreshold){
@@ -560,7 +651,7 @@ void navigateToPositionSmart(float tgtX, float tgtY, geometry_msgs::Twist &vel, 
                 }
                 
             }
-            
+
         }
         // else {
         //     linear = 0.0;
@@ -572,99 +663,12 @@ void navigateToPositionSmart(float tgtX, float tgtY, geometry_msgs::Twist &vel, 
         vel.linear.x = linear;
         vel_pub.publish(vel);
     }
-}
-
-void handleBumperPressed(float turnAngle, geometry_msgs::Twist &vel, ros::Publisher &vel_pub){
-    ROS_INFO("handleBumperPressed() called...");
-    float reverseDistance = 0.2;
-    float forwardDistance = reverseDistance / std::cos(Deg2Rad(turnAngle)) * 0.9;
-
-    float exitDistanceThreshold = 0.02;
-
-    // 1. Reverse
-    ROS_INFO("handleBumperPressed() | Reversing...");
-    float x0 = posX;
-    float y0 = posY;
     
-    float dx;
-    float dy;
-    float d = 0;
-
-    while((d-reverseDistance) < exitDistanceThreshold){
-        ros::spinOnce();
-        ROS_INFO("%.2f", d-reverseDistance);
-        ROS_INFO("%.2f", exitDistanceThreshold);
-
-        linear = -0.1;
-        angular = 0;
-
-        dx = posX-x0;
-        dy = posY-y0;
-        d = (float) sqrt(pow(dx, 2) + pow(dy, 2));
-
-
-
-        vel.angular.z = angular;
-        vel.linear.x = linear;
-        vel_pub.publish(vel);
-    }
-
-
-    // 2. Turn
-    if(turnAngle == 0){ // If center bumper was pressed this is called
-        if(distances.leftRay > distances.rightRay){
-            turnAngle = 45;
-        }
-
-        else{
-            turnAngle = -45;
-        }
-
-    }
-    ROS_INFO("handleBumperPressed() | Turning...");
-    rotateToHeading(yaw + turnAngle, vel, vel_pub);
-
-    // 3. Drive Forward
-    ROS_INFO("handleBumperPressed() | Advancing...");
-    x0 = posX;
-    y0 = posY;
-    dx = 0;
-    dy = 0;
-    d = 0;
-    ROS_INFO("%.2f", d-forwardDistance);
-    while((d-forwardDistance) < exitDistanceThreshold){
-        ros::spinOnce();
-
-        linear = 0.1;
-        angular = 0;
-
-        dx = posX-x0;
-        dy = posY-y0;
-        d = (float) sqrt(pow(dx, 2) + pow(dy, 2));
-
-
-
-        vel.angular.z = angular;
-        vel.linear.x = linear;
-        vel_pub.publish(vel);
-    }
-
-    // 4. Turn Back
-    ROS_INFO("handleBumperPressed() | Correcting yaw...");
-    rotateToHeading(yaw - turnAngle, vel, vel_pub);
-
-
-
-    ROS_INFO("handleBumperPressed() | END");
-    linear = 0;
     angular = 0;
-
+    linear = 0;
     vel.angular.z = angular;
     vel.linear.x = linear;
     vel_pub.publish(vel);
-
-    return;
-
 }
 
 float distanceBetween(float x1, float y1, float x2, float y2){
@@ -783,6 +787,22 @@ void computeZigZag(std::array<std::array<float,2>,4> cornerCoords, int closestIn
     // } 
 }
 
+void checkBumper(geometry_msgs::Twist &vel, ros::Publisher &vel_pub){
+    if(bumpers.anyPressed){
+        if(bumpers.leftPressed){
+            handleBumperPressed((float) -45.0, vel, vel_pub);
+        }
+
+        else if(bumpers.rightPressed){
+            handleBumperPressed((float) 45.0, vel, vel_pub);
+        }
+
+        else if(bumpers.centerPressed){
+            handleBumperPressed((float) 0.0, vel, vel_pub);
+        }
+    }
+}      
+
 
 
 
@@ -809,26 +829,10 @@ int main(int argc, char **argv)
     angular = 0.0;
     linear = 0.0;
 
-    // rotateToHeading(90, vel, vel_pub);
-    // rotateEndlessly(vel, vel_pub);
-    // navigateToPositionSmart(-1.929,1.346, vel, vel_pub);
-    // navigateToPositionSmart(-1.7069999999999999,-1.0, vel, vel_pub);
-    // navigateToPositionSmart(-0.8680000000000001,1.4365, vel, vel_pub);
-    // navigateToPositionSmart(-0.3763333333333332,-1.1406666666666667, vel, vel_pub);
-    // navigateToPositionSmart(0.19299999999999984,1.5270000000000001, vel, vel_pub);
-    // navigateToPositionSmart(0.9543333333333335,-1.2813333333333332, vel, vel_pub);
-    // navigateToPositionSmart(1.2539999999999998,1.6175, vel, vel_pub);
-    // navigateToPositionSmart(2.285,-1.422, vel, vel_pub);
-    // navigateToPositionSmart(2.3149999999999995,1.708, vel, vel_pub);
-
-    // navigateToPositionSmart(-2, 0, vel, vel_pub);
-
-    // return 0;
-
     std::array<std::array<float, 2>, 4> cornerCoords = {{
         {-1.929,1.346},
         {-1.7069999999999999,-1.0},
-        {2.285,-1.422},
+        {2.2,-1.38},
         {2.3149999999999995,1.708}
     }};
 
@@ -855,23 +859,7 @@ int main(int argc, char **argv)
 
 
     while(ros::ok() && secondsElapsed <= 480) {
-        ros::spinOnce();
-        
-
-        if(bumpers.anyPressed){
-            if(bumpers.leftPressed){
-                handleBumperPressed((float) -45.0, vel, vel_pub);
-            }
-
-            else if(bumpers.rightPressed){
-                handleBumperPressed((float) 45.0, vel, vel_pub);
-            }
-
-            else if(bumpers.centerPressed){
-                handleBumperPressed((float) 0.0, vel, vel_pub);
-            }
-        }
-        
+        ros::spinOnce();      
 
         // #pragma region Bumper
         // // Bumper Event
