26e3750783856a0bbd7a3d24069f774de0dcc8eb
osboxes.org <yifeiwumary@outlook.com>
2025-02-10 16:47:53 -0500

    tested simulation

--- src/contest1.cpp
+++ src/contest1.cpp
@@ -4,26 +4,46 @@
 #include "movement.h"
 #include "biasedExplore.h"
 #include "wallFollowing.h"
+#include <ros/ros.h>
+#include <geometry_msgs/PoseStamped.h>
+#include <visualization_msgs/Marker.h>
+
+
+// Define global publishers declared as extern in bumper.h
+ros::Publisher pose_pub;
+ros::Publisher marker_pub;
+
+
+enum Mode {WALL_FOLLOW, RANDOM_NAVIGATE};
+
 
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, "image_listener");
     ros::NodeHandle nh;
 
+
+    // Subscribers
     ros::Subscriber bumper_sub = nh.subscribe("mobile_base/events/bumper", 10, &bumperCallback);
     ros::Subscriber laser_sub = nh.subscribe("scan", 10, &laserCallback);
     ros::Subscriber subOdom = nh.subscribe("odom", 1, &odomCallback);
 
+
+    // Publishers
     ros::Publisher vel_pub = nh.advertise<geometry_msgs::Twist>("cmd_vel_mux/input/teleop", 1);
+    pose_pub = nh.advertise<geometry_msgs::PoseStamped>("bumper_pose", 10);
+    marker_pub = nh.advertise<visualization_msgs::Marker>("visualization_marker", 10);
+
 
     ros::Rate loop_rate(10);
-
     geometry_msgs::Twist vel;
+
 
     // contest count down timer
     std::chrono::time_point<std::chrono::system_clock> start;
     start = std::chrono::system_clock::now();
     uint64_t secondsElapsed = 0;
+
 
     angular = 0.0;
     linear = 0.0;
@@ -31,9 +51,12 @@ int main(int argc, char **argv)
     vel.linear.x = linear;
     vel_pub.publish(vel);
 
+
     std::vector<std::array<float, 2>> sweptPoints;
     std::vector<std::array<float, 2>> visitedPoints;
     float nextX, nextY;
+
+
 
 
     #pragma region wallFollowing Param
@@ -50,15 +73,19 @@ int main(int argc, char **argv)
     int corridor_count = 0;
     bool wall_following = false;
 
+
     // 全局变量：存储上一帧的左/右激光读数
     bool prev_turn = false;
     bool curr_turn = false;
 
+
     float prev_left_distance = 0.0, prev_right_distance = 0.0;
+
 
     float front_dist;
     float left_dist;
     float right_dist;
+
 
     float corridor_threshold = 0.6;   // 设定通道触发阈值
     float max_distance_change = 1.5;  // 设定最大突变距离
@@ -67,146 +94,150 @@ int main(int argc, char **argv)
     float left_change;
     float right_change;
 
+
     std::pair<std::pair<double, double>, std::pair<double, double>> corners;
+
 
     #pragma endregion
     // wallFollowing
-    while(ros::ok() && secondsElapsed <= 180) {
-        ros::spinOnce();
-
-        get_coord();
-
-        corners = filter_corner();
-        // Print corner coord
-        // ROS_INFO("Corner 1: (%.2f, %.2f)", corners.first.first, corners.first.second); 
-        // ROS_INFO("Corner 2: (%.2f, %.2f)", corners.second.first, corners.second.second);
 
 
-        // Check distances
-        front_dist = std::isnan(distances.frontRay) ? safe_threshold : distances.frontRay;
-        left_dist = std::isnan(distances.leftRay) ? safe_threshold : distances.leftRay;
-        right_dist = std::isnan(distances.rightRay) ? safe_threshold : distances.rightRay;
-
-        // ???
-        curr_turn = false;
-
-        // Update linear speed based on wall distances
-        if (distances.min >= safe_threshold) {
-            vel.linear.x = max_speed;
-        }
-        else {
-            vel.linear.x = min_speed + (max_speed - min_speed) * ((distances.min - target_distance) / (safe_threshold - target_distance));
-            vel.linear.x = std::max(static_cast<double>(min_speed), std::min(static_cast<double>(max_speed), vel.linear.x));
-        }
-
-        // Determine if wall is being followed based on left and right distances
-        left_change = left_dist - prev_left_distance;
-        right_change = right_dist - prev_right_distance;
-        if (left_change < 0.1 || right_change < 0.1){
-            wall_following = true;
-        }
+    Mode mode = WALL_FOLLOW;
+    bool fullRoundCompleted = false;
 
 
-        // Main Wall Following Algorithm
-        if (left_change > corridor_threshold & wall_following) {
-            // Calculate rotation angle (clamped between min_rotation and max_rotation)
-            float rotation_angle_deg = min_rotation + (left_change - corridor_threshold) * (max_rotation - min_rotation) / (max_distance_change - corridor_threshold);
-            rotation_angle_deg = std::min(max_rotation, std::max(min_rotation, rotation_angle_deg)); // Clamp the angle
-            float rotation_angle_rad = Deg2Rad(rotation_angle_deg); // Convert to radians
-
-            ROS_WARN("Detected corridor on the LEFT! Turning %.1f°", rotation_angle_deg);
-            
-            vel.angular.z = 0.0;  // No adjustment needed
-
-            // Initialize current position if this is the first corridor detection
-            if (corridor_count < 1) {
-                current_x = posX;
-                current_y = posY;
-                corridor_count += 1;
-            }
-
-            moveRobot(distances.leftVertPrev, 0, vel, vel_pub);
-            ROS_WARN("front distance move %.1f°", distances.leftVertPrev);
-            wall_following = false;
-        }
-
-        else if (right_change > corridor_threshold & wall_following) {
-            // 计算旋转角度 (限制 30° ~ 90°)
-            float rotation_angle_deg = min_rotation + (right_change - corridor_threshold) * (max_rotation - min_rotation) / (max_distance_change - corridor_threshold);
-            rotation_angle_deg = std::min(max_rotation, std::max(min_rotation, rotation_angle_deg));
-            float rotation_angle_rad = Deg2Rad(rotation_angle_deg);
-
-            ROS_WARN("Detected corridor on the RIGHT! Turning %.1f°", rotation_angle_deg);
-            wall_following = false;
-        }
-
-        else {
-            // Choose the wall to follow (LEFT or RIGHT)
-            WallSide wall_side = LEFT; // Change to RIGHT to follow the right wall
-            // ros::Publisher vel_pub = nh.advertise<geometry_msgs::Twist>("cmd_vel_mux/input/teleop", 1);
-
-            // Call the wall-following function
-            wallFollowing(wall_side, curr_turn, prev_turn, left_dist, right_dist, front_dist, 
-              target_distance, min_speed, k, alpha, vel, vel_pub);
-        }
-
-        prev_left_distance = left_dist;
-        prev_right_distance = right_dist;
-        prev_turn = curr_turn;
-
-        vel_pub.publish(vel);
-
-
-        // Loop Checker
-        if (is_position_visited(posX, posY)) {
-            ROS_INFO("Robot has completed a round and returned to previous position.");
-            
-            //get_all_corners
-            std::vector<std::pair<double, double>> corners = get_all_corners();
-            
-            // Printing out the corners for debugging or monitoring
-            ROS_INFO("Detected Corners:");
-            for (const auto& corner : corners) {
-                ROS_INFO("Corner: (X: %.2f, Y: %.2f)", corner.first, corner.second);
-            }
-
-	        break;  // Stop the loop if visited position
-        }
-
-
-
-
-        secondsElapsed = std::chrono::duration_cast<std::chrono::seconds>(std::chrono::system_clock::now()-start).count();
-        loop_rate.sleep();
-    }
-
-
-
-    // biasedExplore
     while(ros::ok() && secondsElapsed <= 480) {
         ros::spinOnce();
 
-        sweptPoints.clear();
-        sweep360(sweptPoints, vel, vel_pub);
-        ROS_INFO("Size: %zu", sweptPoints.size());
-        
-        findNextDestination(posX, posY, sweptPoints, visitedPoints, nextX, nextY);
 
-        ROS_INFO("----------------Visited Positions----------------");
-        for(int i = 0; i < visitedPoints.size(); i++){
-            ROS_INFO("(%.2f,%.2f)", visitedPoints[i][0], visitedPoints[i][1]);
+        switch (mode) {
+            case WALL_FOLLOW: {
+
+                get_coord();
+                corners = filter_corner();
+
+                // Check distances
+                front_dist = std::isnan(distances.frontRay) ? safe_threshold : distances.frontRay;
+                left_dist = std::isnan(distances.leftRay) ? safe_threshold : distances.leftRay;
+                right_dist = std::isnan(distances.rightRay) ? safe_threshold : distances.rightRay;
+
+                curr_turn = false;
+
+                // Update linear speed based on wall distances
+                if (distances.min >= safe_threshold) {
+                    vel.linear.x = max_speed;
+                }
+                else {
+                    vel.linear.x = min_speed + (max_speed - min_speed) * ((distances.min - target_distance) / (safe_threshold - target_distance));
+                    vel.linear.x = std::max(static_cast<double>(min_speed), std::min(static_cast<double>(max_speed), vel.linear.x));
+                }
+
+
+                // Determine if wall is being followed based on left and right distances
+                left_change = left_dist - prev_left_distance;
+                right_change = right_dist - prev_right_distance;
+                if (left_change < 0.1 || right_change < 0.1){
+                    wall_following = true;
+                }
+
+                // Main Wall Following Algorithm
+                if (left_change > corridor_threshold && wall_following) {
+                    // Calculate rotation angle (clamped between min_rotation and max_rotation)
+                    float rotation_angle_deg = min_rotation + (left_change - corridor_threshold) * (max_rotation - min_rotation) / (max_distance_change - corridor_threshold);
+                    rotation_angle_deg = std::min(max_rotation, std::max(min_rotation, rotation_angle_deg)); // Clamp the angle
+                    float rotation_angle_rad = Deg2Rad(rotation_angle_deg); // Convert to radians
+
+                    ROS_WARN("Detected corridor on the LEFT! Turning %.1f°", rotation_angle_deg);
+                   
+                    vel.angular.z = 0.0;  // No adjustment needed
+
+                    // Initialize current position if this is the first corridor detection
+                    if (corridor_count < 1) {
+                        current_x = posX;
+                        current_y = posY;
+                        corridor_count += 1;
+                    }
+
+
+                    moveRobot(distances.leftVertPrev, 0, vel, vel_pub);
+                    ROS_WARN("front distance move %.1f°", distances.leftVertPrev);
+                    wall_following = false;
+                }
+
+
+                else if (right_change > corridor_threshold && wall_following) {
+                    // 计算旋转角度 (限制 30° ~ 90°)
+                    float rotation_angle_deg = min_rotation + (right_change - corridor_threshold) * (max_rotation - min_rotation) / (max_distance_change - corridor_threshold);
+                    rotation_angle_deg = std::min(max_rotation, std::max(min_rotation, rotation_angle_deg));
+                    float rotation_angle_rad = Deg2Rad(rotation_angle_deg);
+
+
+                    ROS_WARN("Detected corridor on the RIGHT! Turning %.1f°", rotation_angle_deg);
+                    wall_following = false;
+                }
+
+
+                else {
+                    // Choose the wall to follow (LEFT or RIGHT)
+                    WallSide wall_side = LEFT; // Change to RIGHT to follow the right wall
+                    // ros::Publisher vel_pub = nh.advertise<geometry_msgs::Twist>("cmd_vel_mux/input/teleop", 1);
+
+
+                    // Call the wall-following function
+                    wallFollowing(wall_side, curr_turn, prev_turn, left_dist, right_dist, front_dist,
+                    target_distance, min_speed, k, alpha, vel, vel_pub);
+                }
+
+
+                prev_left_distance = left_dist;
+                prev_right_distance = right_dist;
+                prev_turn = curr_turn;
+
+
+                vel_pub.publish(vel);
+
+
+                // Loop Checker
+                if (is_position_visited(posX, posY)) {
+                    ROS_INFO("Robot has completed a round and returned to previous position.");
+                    fullRoundCompleted = true;
+                    mode = RANDOM_NAVIGATE;  // Transition to random navigation
+                    break;  // Stop wall-following
+                }
+
+
+                break;
+            }
+            case RANDOM_NAVIGATE: {
+                sweptPoints.clear();
+                sweep360(sweptPoints, vel, vel_pub);
+                ROS_INFO("Size: %zu", sweptPoints.size());
+               
+                findNextDestination(posX, posY, sweptPoints, visitedPoints, nextX, nextY);
+
+
+                ROS_INFO("----------------Visited Positions----------------");
+                for(int i = 0; i < visitedPoints.size(); i++){
+                    ROS_INFO("(%.2f,%.2f)", visitedPoints[i][0], visitedPoints[i][1]);
+                }
+
+
+                navigateToPosition(nextX, nextY, vel, vel_pub);  
+               
+                break;
+            }
         }
 
-        navigateToPosition(nextX, nextY, vel, vel_pub);      
-
-        // vel.angular.z = angular;
-        // vel.linear.x = linear;
-        // vel_pub.publish(vel);
-
-        // The last thing to do is to update the timer.
         secondsElapsed = std::chrono::duration_cast<std::chrono::seconds>(std::chrono::system_clock::now()-start).count();
         loop_rate.sleep();
     }
 
+
     return 0;
 }
+
+
+
+
+
+
